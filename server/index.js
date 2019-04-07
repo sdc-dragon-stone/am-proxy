82264a061b521da9a52fa6c41f4f07363380d80f
Mike <mikeglopez.91@gmail.com>
2019-04-07 15:50:08 -0500

    Render all modules on the page

--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const httpProxy = require('http-proxy');
 
+const apiProxy = httpProxy.createProxyServer();
 const app = express();
 const port = 3010;
 
+// app.set('trust proxy', true);
 app.use(express.static('./public'));
 
 // const randNum = () => Math.floor(Math.random() * Math.floor(100));
 
-app.get('/', (req, res) => {
-  res.status(200).send('This is the proxy server!');
+app.get('/description', (req, res) => {
+  apiProxy.web(req, res, { target: 'http://localhost:3210' });
+});
+
+app.get('/totalReviews', (req, res) => {
+  apiProxy.web(req, res, { target: 'http://localhost:3004' });
+});
+
+app.get('/totalReviews', (req, res) => {
+  apiProxy.web(req, res, { target: 'http://localhost:3004' });
+});
+
+app.get('/allHomes', (req, res) => {
+  apiProxy.web(req, res, { target: 'http://localhost:3000' });
+});
+
+app.get('/moreHomes', (req, res) => {
+  apiProxy.web(req, res, { target: 'http://localhost:3000' });
 });
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
