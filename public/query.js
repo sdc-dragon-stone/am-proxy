50680f0d9db7491ef6bd6fa532268387b7756e8e
Mike <mikeglopez.91@gmail.com>
2019-04-11 10:31:33 -0500

    Fix the random id function to generate ids 1 - 100

--- public/query.js
+++ public/query.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get('id');
 
 if (id === null) {
-  id = Math.floor(Math.random() * 10) + 1;
+  id = Math.floor(Math.random() * 100) + 1;
 }
 
 window.id = id;
