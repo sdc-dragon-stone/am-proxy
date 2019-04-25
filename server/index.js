b22ba65af117d520b0e88713d2e9fbbab59e5d03
Mike <mikeglopez.91@gmail.com>
2019-04-24 23:20:29 -0500

    Comment offline reviews service out

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.get('/description', (req, res) => {
   apiProxy.web(req, res, { target: 'http://18.220.111.23:3210/' });
 });
 
-app.get('/totalReviews', (req, res) => {
-  apiProxy.web(req, res, { target: 'http://reviews.pfuzgfpajh.us-west-2.elasticbeanstalk.com/' });
-});
+// app.get('/totalReviews', (req, res) => {
+//   apiProxy.web(req, res, { target: 'http://reviews.pfuzgfpajh.us-west-2.elasticbeanstalk.com/' });
+// });
 
 app.get('/moreHomes', (req, res) => {
   apiProxy.web(req, res, { target: 'http://52.88.112.117:3000/' });
