2d9b15f4e071db88dc10bc546bfc3a07a5b3ce99
Mike <mikeglopez.91@gmail.com>
2019-04-27 15:01:35 -0500

    Testing

--- server/index.js
+++ server/index.js
@@ -7,20 +7,20 @@ const port = 3010;
 
 app.use(express.static('./public'));
 
-app.get('/house_images', (req, res) => {
-  apiProxy.web(req, res, { target: 'http://ec2-54-175-40-167.compute-1.amazonaws.com:3003/' });
-});
+// app.get('/house_images', (req, res) => {
+//   apiProxy.web(req, res, { target: 'http://ec2-54-175-40-167.compute-1.amazonaws.com:3003/' });
+// });
 
 app.get('/description', (req, res) => {
   apiProxy.web(req, res, { target: 'http://18.220.111.23:3210/' });
 });
 
-// app.get('/totalReviews', (req, res) => {
-//   apiProxy.web(req, res, { target: 'http://reviews.pfuzgfpajh.us-west-2.elasticbeanstalk.com/' });
-// });
+app.get('/totalReviews', (req, res) => {
+  apiProxy.web(req, res, { target: 'http://reviews.pfuzgfpajh.us-west-2.elasticbeanstalk.com/' });
+});
 
 app.get('/moreHomes', (req, res) => {
-  apiProxy.web(req, res, { target: 'http://52.88.112.117:3000/' });
+  apiProxy.web(req, res, { target: 'http://52.27.3.181:3000/' });
 });
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
