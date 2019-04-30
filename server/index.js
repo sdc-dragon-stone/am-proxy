64ab520b743b98fe16c82734570c9a4d92b8e422
Mike <mikeglopez.91@gmail.com>
2019-04-30 12:26:39 -0500

    Test the images service alone

--- server/index.js
+++ server/index.js
@@ -11,17 +11,17 @@ app.get('/house_images', (req, res) => {
   apiProxy.web(req, res, { target: 'http://52.87.228.239:3003/' });
 });
 
-app.get('/description', (req, res) => {
-  apiProxy.web(req, res, { target: 'http://18.188.174.153:3210/' });
-});
+// app.get('/description', (req, res) => {
+//   apiProxy.web(req, res, { target: 'http://18.188.174.153:3210/' });
+// });
 
-app.get('/totalReviews', (req, res) => {
-  apiProxy.web(req, res, { target: 'http://52.10.65.36/' });
-});
+// app.get('/totalReviews', (req, res) => {
+//   apiProxy.web(req, res, { target: 'http://52.10.65.36/' });
+// });
 
-app.get('/moreHomes', (req, res) => {
-  apiProxy.web(req, res, { target: 'http://52.27.3.181:3000/' });
-});
+// app.get('/moreHomes', (req, res) => {
+//   apiProxy.web(req, res, { target: 'http://52.27.3.181:3000/' });
+// });
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
